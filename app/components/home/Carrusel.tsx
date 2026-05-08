3a79eae71138ca67d3129987f6188ea4f09814d3
tomas <totobarajas124@gmail.com>
2026-05-08 14:53:19 -0300

    feat: update responsive banner carousel component with optimized image assets

--- app/components/home/Carrusel.tsx
+++ app/components/home/Carrusel.tsx
@@ -2,9 +2,9 @@ export default function Carrusel() {
   return (
     <section>
       <picture>
-        <source media="(max-width: 768px)" srcSet="/bannermobile.png" />
-        <source media="(min-width: 769px)" srcSet="/banner.jpeg" />
-        <img src="/banner.jpeg" className="w-full" alt="Carrusel" />
+        <source media="(max-width: 768px)" srcSet="/bannermobile.jpg" />
+        <source media="(min-width: 769px)" srcSet="/banner.jpg" />
+        <img src="/banner.jpg" className="w-full" alt="Carrusel" />
       </picture>
     </section>
   );

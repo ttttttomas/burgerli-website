b5758e1378f19963b2e2b1a26c89cb8d6e2e600f
tomas <totobarajas124@gmail.com>
2026-05-07 21:14:54 -0300

    Update: updated image banner mobile

--- app/components/home/Carrusel.tsx
+++ app/components/home/Carrusel.tsx
@@ -2,7 +2,7 @@ export default function Carrusel() {
   return (
     <section>
       <picture>
-        <source media="(max-width: 768px)" srcSet="/bannermobile.jpeg" />
+        <source media="(max-width: 768px)" srcSet="/bannermobile.png" />
         <source media="(min-width: 769px)" srcSet="/banner.jpeg" />
         <img src="/banner.jpeg" className="w-full" alt="Carrusel" />
       </picture>

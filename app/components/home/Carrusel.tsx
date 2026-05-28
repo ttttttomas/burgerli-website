e2d6cff8e1e8ea020afa7902e139a2f7d01e800d
tomas <totobarajas124@gmail.com>
2026-05-28 08:04:52 -0300

    feat: add responsive Carrusel component with Smarttv and banner image support

--- app/components/home/Carrusel.tsx
+++ app/components/home/Carrusel.tsx
@@ -2,7 +2,7 @@ export default function Carrusel() {
   return (
     <section>
       <picture>
-        <source media="(max-width: 768px)" srcSet="/bannermobile.jpg" />
+        <source media="(max-width: 768px)" srcSet="/Smarttv.png" />
         <source media="(min-width: 769px)" srcSet="/banner.jpg" />
         <img src="/banner.jpg" className="w-full" alt="Carrusel" />
       </picture>
